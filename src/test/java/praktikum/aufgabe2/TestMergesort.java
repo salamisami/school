9a1d3e5ca5c1cf692e159b613b98fd5751a7f389
Sami Askar <sami.askar@haw-hamburg.de>
2022-04-23 21:25:34 +0200

    added a new testclass

---
+++ src/test/java/praktikum/aufgabe2/TestMergesort.java
@@ -0,0 +1,11 @@
+package praktikum.aufgabe2;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMergesort {
+    @Test
+    public void testIntegerLinkedList(){
+        Mergesort mergesort = new Mergesort()
+                //TODO: add tests for list of integer, custom classes, strings...
+    }
+}
