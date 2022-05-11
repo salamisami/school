529e180dcf04327b9871a774b6d39b4fb996f1ca
Sami Askar <sami.askar@haw-hamburg.de>
2022-05-11 02:45:59 +0200

    Fixed sorting, merging and print issues.

--- src/main/java/praktikum/aufgabe2/Constants.java
+++ src/main/java/praktikum/aufgabe2/Constants.java
@@ -6,5 +6,5 @@ package praktikum.aufgabe2;
  */
 public class Constants {
 
-    public static final int SIZE_OF_TO_SORT = 10;
+    public static final int SIZE_OF_TO_SORT = 500;
 }
--- src/main/java/praktikum/aufgabe2/Mergesort.java
+++ src/main/java/praktikum/aufgabe2/Mergesort.java
@@ -1,27 +1,46 @@
 package praktikum.aufgabe2;
 
 import java.lang.reflect.Array;
+import java.sql.SQLOutput;
 import java.util.*;
+
+import static praktikum.aufgabe2.Constants.SIZE_OF_TO_SORT;
 
 public class Mergesort<T> {
 
     public static void main(String[] args){
         Mergesort myMerge = new Mergesort<Integer>();
-        ArrayList<Integer> sortMe = myMerge.randomArrayList(Constants.SIZE_OF_TO_SORT);
+        ArrayList<Integer> sortMe = myMerge.randomArrayList(SIZE_OF_TO_SORT);
         Comparator<Integer> comparator = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o1-o2;
             }
         };
-        myMerge.mergeSort(sortMe, comparator);
+        System.out.println("The unsorted list:");
+        for (int i = 0; i < sortMe.size(); i++) {
+            System.out.print(String.format("%d. %d; ", i+1, sortMe.get(i)));
+            if ((i+1)%10==0){
+                System.out.println();
+            }
+        }
+        System.out.println("");
+        List<Integer> result = myMerge.mergeSort(sortMe, comparator);
+        System.out.println("The sorted list:");
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(String.format("%d. %d; ", i+1, result.get(i)));
+            if ((i+1)%10==0){
+                System.out.println();
+            }
+        }
     }
     private ArrayList<Integer> randomArrayList (int size){
         Random rd = new Random();
 
     ArrayList<Integer> arr = new ArrayList<Integer>();
       for (int i = 0; i < size; i++) {
-        arr.add(rd.nextInt()); // storing random integers in an array
+          arr.add((int)Math.floor(Math.random()*(SIZE_OF_TO_SORT*4))); //max,min, +1, min
+        //arr.add(rd.nextInt()); // storing random integers in an array
     }
       return  arr;
 }
@@ -30,6 +49,7 @@ public class Mergesort<T> {
         if (maxLength<2){
             return list;
         }
+        //TODO: Insitu implementieren.
         LinkedList<T> left = new LinkedList<>();
         LinkedList<T> right = new LinkedList<>();
         int midIndex = maxLength/2;
@@ -56,16 +76,16 @@ public class Mergesort<T> {
         int leftNumbersOver = leftLength - counterLeft; //Wie viele sind unsortiert?
         int rightNumbersOver = rightLength - counterRight;
         while (leftNumbersOver > 0 || rightNumbersOver > 0){
-            leftNumbersOver = leftLength - counterLeft; //Wie viele sind unsortiert?
-            rightNumbersOver = rightLength - counterRight;
-            if (leftNumbersOver < 1){
+            if (leftNumbersOver < 1){// TODO: Schöner lösen, counters vereinfachen.
                 result.add(right.get(counterRight));
-                counterRight++;
+                counterRight++;//Wie viele sind unsortiert?
+                rightNumbersOver = rightLength - counterRight;
                 continue;
             }
             if (rightNumbersOver < 1){
                 result.add(left.get(counterLeft));
                 counterLeft++;
+                leftNumbersOver = leftLength - counterLeft; //Wie viele sind unsortiert?
                 continue;
             }
             int compareResult = comparator.compare(left.get(counterLeft), right.get(counterRight));
@@ -77,6 +97,8 @@ public class Mergesort<T> {
                 result.add(right.get(counterRight));
                 counterRight++;
             }
+            leftNumbersOver = leftLength - counterLeft; //Wie viele sind unsortiert?
+            rightNumbersOver = rightLength - counterRight;
         }
         return result;
     }
